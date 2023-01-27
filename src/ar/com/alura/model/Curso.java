703c5bf733503094c4b77c93b86dc5a78bd0c81b
lokywolf2295 <matiasesek1995@gmail.com>
2023-01-27 13:23:36 -0300

    I created Lecture 4 to practice with packages and classes

---
+++ src/ar/com/alura/model/Curso.java
@@ -0,0 +1,39 @@
+
+package ar.com.alura.model;
+
+public class Curso {
+
+    //Creo Mis variables privadas
+    private String nombre;
+    private int tiempo;
+    
+    //creo el constructor con sus parametros
+    public Curso(String nombre, int tiempo) {
+        this.nombre = nombre;
+        this.tiempo = tiempo;
+    }
+
+    //creo los metodos getter and setter
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getTiempo() {
+        return tiempo;
+    }
+
+    public void setTiempo(int tiempo) {
+        this.tiempo = tiempo;
+    }
+
+    //modifico el metodo toStrong de su clase padre
+    @Override
+    public String toString(){
+        return this.nombre;
+    }
+
+}
